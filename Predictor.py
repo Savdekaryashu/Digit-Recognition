3464fe64baaea25f831d6670a1406c93d0602d7f
Yash Savdekar <yashusavdekar32@gmail.com>
2024-10-14 05:02:03 +0530

    enabled Drawing

--- Predictor.py
+++ Predictor.py
@@ -5,12 +5,23 @@ matplotlib.use('TkAgg')
 from matplotlib.widgets import Button
 import tensorflow as tf
 
-model=tf.keras.models.load_model('Models/model_4_layer.h5')
+model=tf.keras.models.load_model('Models/model_2_layer.h5')
 
 canvas=np.zeros((28,28))
+drawing=False
+
+def start_drawing(event):
+    global drawing
+    drawing = True
+    draw(event)  # Draw at the current position
+
+# Function to stop drawing on button release
+def stop_drawing(event):
+    global drawing
+    drawing = False
 
 def draw(event):
-    if event.xdata is not None and event.ydata is not None:
+    if drawing and event.xdata is not None and event.ydata is not None:
         x=int(event.xdata)
         y=int(event.ydata)
         if 0 <= x < 28 and 0 <= y < 28:
@@ -39,14 +50,21 @@ ax.imshow(canvas,cmap='gray')
 ax.set_xticklabels([])
 ax.set_yticklabels([])
 
-cid = fig.canvas.mpl_connect('motion_notify_event', draw)
+cid_press = fig.canvas.mpl_connect('button_press_event', start_drawing)
+cid_release = fig.canvas.mpl_connect('button_release_event', stop_drawing)
+cid_motion = fig.canvas.mpl_connect('motion_notify_event', draw)
+
 
 clear_button_ax=plt.axes([0.5, 0.05, 0.3, 0.075])
 clear_btn=Button(clear_button_ax,'Clear')
 clear_btn.on_clicked(clear_canvas)
 
 predict_button_ax=plt.axes([0.1, 0.05, 0.3, 0.075])
-predict_button_ax=Button(predict_button_ax,'Predict')
-predict_button_ax.on_clicked(predict)
+predict_Butt=Button(predict_button_ax,'Predict')
+predict_Butt.on_clicked(predict)
 
 plt.show()
+
+fig.canvas.mpl_disconnect(cid_press)
+fig.canvas.mpl_disconnect(cid_release)
+fig.canvas.mpl_disconnect(cid_motion)
