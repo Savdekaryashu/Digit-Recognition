594ad0c610d601b65d79e7a462aaffb34fa33412
Yash Savdekar <yashusavdekar32@gmail.com>
2024-10-14 04:06:56 +0530

    model training
    in this we trained the model

---
+++ NN.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+minst=tf.keras.datasets.mnist
+
+(X, Y), (x,y) = minst.load_data()
+
+X=X/255.0
+x=x/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(256, activation='relu'),
+                                    tf.keras.layers.Dense(128, activation='sigmoid'),
+                                    tf.keras.layers.Dense(64, activation='sigmoid'),
+                                    tf.keras.layers.Dense(10, activation='softmax')
+                                    ])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x,y,epochs=15)
+validation_data=(x,y)
+
+test_loss, test_acc = model.evaluate(x, y)
+print(f'Test accuracy: {test_acc}')
